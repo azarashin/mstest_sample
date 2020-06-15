87da3f5c3d7ea4fa33d026b00f1cfda06ff1be03
azarashin <azarashin1979@gmail.com>
2020-06-15 18:04:39 +0900

    1st import.

---
+++ sample/console/Program.cs
@@ -0,0 +1,18 @@
+﻿using core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 c = new Class1();
+            c.Hello(); 
+        }
+    }
+}
